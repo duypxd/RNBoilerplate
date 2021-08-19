f99a89900f82ef96aae1989e8e00a13d4464909e
xuanduyrn <duy.pham@team.enouvo.com>
2021-08-19 16:31:08 +0700

    update config + sentry + rn-check-version

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
+import Config from 'react-native-config';
 import App from './src';
 import { name as appName } from './app.json';
+import * as Sentry from '@sentry/react-native';
+
+if (!__DEV__) {
+  Sentry.init({
+    dsn: Config.SENTRY_DSN,
+    environment: Config.APP_ENVIRONMENT,
+  });
+}
 
 AppRegistry.registerComponent(appName, () => App);
