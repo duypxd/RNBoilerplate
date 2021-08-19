a9eedb6b0d9949b68c98c554885cd319845b1622
xuanduyrn <duy.pham@team.enouvo.com>
2021-08-19 17:29:55 +0700

    add Provider redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppearanceProvider } from 'react-native-appearance';
+import { store, persistor } from './redux/store';
 import NavigationContainer from './navigation';
 import { ThemeContext, ThemeProvider } from './themes/ThemeContext';
 
 const App = () => {
   return (
-    <AppearanceProvider>
-      <ThemeProvider>
-        <ThemeContext.Consumer>
-          {contexts => <NavigationContainer {...contexts} />}
-        </ThemeContext.Consumer>
-      </ThemeProvider>
-    </AppearanceProvider>
+    <Provider store={store}>
+      <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+        <AppearanceProvider>
+          <ThemeProvider>
+            <ThemeContext.Consumer>
+              {contexts => <NavigationContainer {...contexts} />}
+            </ThemeContext.Consumer>
+          </ThemeProvider>
+        </AppearanceProvider>
+      </PersistGate>
+    </Provider>
   );
 };
 
